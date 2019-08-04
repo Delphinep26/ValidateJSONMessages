0a3606c60cfc544952266dfe4fd95829e89f5055
delphinep26 <delphine-elfassi@hotmail.com>
2019-08-04 21:50:48 +0300

    Final

--- Sql Task/Task_2_with_only_cte.sql
+++ Sql Task/Task_2_with_only_cte.sql
@@ -1,25 +1,27 @@
-SELECT m.*
-INTO first_bulk_tmp
-	FROM (
-	SELECT 
-		     m.type,m.msg,m.id,
-			 ROW_NUMBER() OVER(ORDER BY id) AS row_num,
-             LEAD(type) OVER (ORDER BY id) as next_type,
-             LEAD(msg) OVER (ORDER BY id) as next_msg,
-             SUM(CASE WHEN type = 0 THEN 1 ELSE 0 END) OVER (ORDER BY id) AS num_0s
-      FROM Messages m
-	  ) m
-WHERE num_0s = 1 
-
-BEGIN 
-IF NOT EXISTS(SELECT 1 FROM first_bulk_tmp WHERE row_num != id)
-   PRINT 'Missing ID in the first bulk' 
-ELSE 
-
 BEGIN
-   SELECT 
-	REPLACE(CONCAT(msg, CASE WHEN next_type = 84 THEN next_msg ELSE '' END),' ','') 
-		FROM first_bulk_tmp
-		WHERE TYPE = 83
-	END
+ 
+WITH limits AS
+(
+	SELECT MIN(id) as id01, MAX(id) AS id02
+	FROM (SELECT TOP 2 id, type from messages WHERE type = 0 ORDER BY id) first2_0s
+)
+, first_bulk AS (
+	SELECT m.* FROM (
+			SELECT ID, ROW_NUMBER() OVER (ORDER BY ID) AS ROW_NUM,
+			type, LEAD(type) over(ORDER BY id) AS next_type,
+			msg, lead(msg) over(ORDER BY id) AS next_msg  
+			FROM messages m
+	) m
+	WHERE id > (SELECT id01 from limits)
+    AND id < (SELECT id02 from limits)
+)
+SELECT 
+	CASE 
+		WHEN not exists(SELECT 1 FROM first_bulk WHERE row_num != id) THEN 
+		REPLACE ( CONCAT (msg, CASE WHEN next_type = 84 THEN next_msg ELSE '' END),' ','') 
+		ELSE 'Missing ID in the first bulk'		
+	END 
+
+FROM first_bulk
+WHERE TYPE = 83
 END
